c94ee72c46910290cf8fc210d2bc337480df0bc2
Fran <fcutanda@hepoluz.es>
2023-11-30 12:09:08 +0100

    form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // Write one title and textbox to URL data and a button
+
+  const title = 'Computer Vision';
+  return <div>
+        <h1>{title}</h1>
+        <label for="url-input">Insert URL or type pronpt:</label>
+        <input id="url-input" type="text" placeholder='Enter URL to analyze or textual promppt to generate an image' />
+        <button>Analyze</button>
+        <button>Generate</button>
+        </div>;
 }
 
 export default App;
